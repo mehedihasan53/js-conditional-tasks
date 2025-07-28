09187ef39cf804e5695a717ba6a4c93736df1531
mehedihasan53 <mehedihasan.codes3@gmail.com>
2025-07-28 16:42:04 +0600

    task_3_grade_calculator

---
+++ t3-grade_calculator.js
@@ -0,0 +1,31 @@
+/***
+Grade Calculator
+Create a simple JavaScript program that takes a student's score as input 
+and returns their corresponding grade based on the following grading scale:
+***/
+
+var sub=32;
+
+if(sub>=80){ 
+    console.log("A+");
+}
+else if(sub>=70){
+    console.log("A");
+}
+else if(sub>=60){
+    console.log("A-");
+}
+else if(sub>=50){
+    console.log("B");
+}
+else if(sub>=40){
+    console.log("C");
+}
+else if(sub>=33){
+    console.log("D");
+}
+else{
+    console.log("F");
+}
+
+
