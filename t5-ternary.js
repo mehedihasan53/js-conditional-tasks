6d424ccdaea3e476cfe608f9f9b8ae80ce3fafac
mehedihasan53 <mehedihasan.codes3@gmail.com>
2025-07-29 13:42:21 +0600

    task_5_ternary

--- t5-ternary.js
+++ t5-ternary.js
@@ -13,7 +13,7 @@ also, write it using ternary operator.
  */
 
 let num1 = 10;
-let num2 = 2;
+let num2 = 20;
 let result;
 if (num1 > num2) {
     result = num1 * 2;
@@ -21,4 +21,9 @@ if (num1 > num2) {
 else {
     result = num1 + num2;
 }
+console.log(result);
+
+/* ternary operator */
+
+result = num1 > num2 ? num1 * 2 : num1 + num2;
 console.log(result);
